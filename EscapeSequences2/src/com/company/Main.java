f46f57aec61fc138c0d001acc9386b83ad6946fc
Rekha Mundhra <rmundhra2003@yahoo.com>
2019-04-17 13:06:39 -0400

    EscapeSequences2

---
+++ EscapeSequences2/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("\tCost\tQuantity\tTotal");
+        System.out.println("\t$1000.00\t4\t\t$4000.00");
+        System.out.println("\t$50.00\t    8\t\t$400.00");
+        System.out.println("\t______________________________");
+        System.out.println("\tTOTAL:\t\t12\t\t$4,400.00");
+    }
+}
