6920702561a4bc9c37dadbdf0ccae0f3a10d5d3a
Rekha Mundhra <rmundhra2003@yahoo.com>
2019-04-17 11:23:24 -0400

    EscapeSequences

---
+++ EscapeSequences/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("\t*********");
+        System.out.println("\t*\t\t*");
+        System.out.print("\r");
+        System.out.println("\t*\t\t*");
+        System.out.print("\r");
+        System.out.println("\t*\t\t*");
+        System.out.print("\r");
+        System.out.println("\t*\t\t*");
+        System.out.print("\r");
+        System.out.println("\t*********");
+        System.out.println("\r");
+
+
+        System.out.println("\t   ***");
+        System.out.println("\t  *\t  *");
+        System.out.println("\t *\t   *");
+        System.out.println("\t *\t   *");
+        System.out.println("\t  *\t  *");
+        System.out.println("\t   ***");
+        System.out.println("\r");
+
+        System.out.println("\t\t*");
+        System.out.println("\t   * *");
+        System.out.println("\t  *   *");
+        System.out.println("\t   * *");
+        System.out.println("\t\t*");
+    }
+}
